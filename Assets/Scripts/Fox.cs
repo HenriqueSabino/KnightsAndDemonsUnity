6f3449d62cb9ce0696ccdf828bc0704789b4e104
HenriqueSabino <h.sabinocosta@gmail.com>
2022-05-01 12:22:52 -0300

    Separating collisions

--- Assets/Scripts/Fox.cs
+++ Assets/Scripts/Fox.cs
@@ -10,6 +10,7 @@ public class Fox : MonoBehaviour
     [SerializeField]
     private new Rigidbody2D rigidbody2D;
     private SpriteRenderer sprite;
+    private bool TakingDamage;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +22,31 @@ public class Fox : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        int sign = (int)Mathf.Sign(Target.position.x - transform.position.x);
+        if (!TakingDamage)
+        {
+            int sign = (int)Mathf.Sign(Target.position.x - transform.position.x);
 
-        sprite.flipX = sign > 0;
+            sprite.flipX = sign > 0;
 
-        rigidbody2D.velocity = new Vector2(sign * Speed, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = new Vector2(sign * Speed, rigidbody2D.velocity.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            TakingDamage = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == 8)
         {
-            rigidbody2D.AddForce(new Vector2(0, 10));
-            print("hit");
+            TakingDamage = true;
+            int sign = (int)Mathf.Sign(Target.position.x - transform.position.x);
+            rigidbody2D.AddForce(new Vector2(sign * 2, 2));
         }
     }
 }
