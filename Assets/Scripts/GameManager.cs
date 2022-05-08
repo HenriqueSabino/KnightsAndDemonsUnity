c157f20ae0447e80c1c40fb0c01bed4009c09222
Nelson Lins <54917418+NelsuLins@users.noreply.github.com>
2022-05-08 02:06:08 -0300

    Hud and Scenario Sprites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -8,7 +9,13 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
-    public int PlayerDeaths;
+    public int PlayerLives;
+
+    public int health;
+
+    public int heathmax;
+
+    public Image HealthBar;
 
     void Awake()
     {
@@ -16,8 +23,21 @@ public class GameManager : MonoBehaviour
             instance = this;
     }
 
+    private void Update() 
+    {
+        AttLife();
+    }
+
     public void PlayerDeath() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void AttLife()
+    {
+        if(Player.instance.Health > 0)
+            HealthBar.transform.localScale = new Vector3(Player.instance.Health,1,0);
+        else
+            HealthBar.transform.localScale = new Vector3(0,1,0);
     }
 }
